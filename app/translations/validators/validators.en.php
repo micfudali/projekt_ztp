769bfbdac1ea548a2f0032972436dbbb0eb358f2
Michał <you@example.com>
2024-06-08 15:32:01 +0200

    Added missing translation

--- app/translations/validators/validators.en.php
+++ app/translations/validators/validators.en.php
@@ -3,5 +3,6 @@
 return [
     'message.this_must_be_an_email' => 'There must be an e-mail here.',
     'message.not_blank' => 'This field cannot be blank.',
-    'message.min_length' => 'The new password should be at least 6 characters.'
+    'message.min_length' => 'The new password should be at least 6 characters.',
+    'validator.there_is_already_an_account_with_this_email' => 'There is already an account with this email.',
 ];
--- app/translations/validators/validators.pl.php
+++ app/translations/validators/validators.pl.php
@@ -3,5 +3,6 @@
     return [
     'message.this_must_be_an_email' => 'Tu należy podać e-mail',
         'message.not_blank' => 'To pole nie może być puste.',
-        'message.min_length' => 'Nowe hasło musi mieć co najmniej 6 znaków.'
+        'message.min_length' => 'Nowe hasło musi mieć co najmniej 6 znaków.',
+        'validator.there_is_already_an_account_with_this_email' => 'Istnieje już konto z tym e-mailem.'
     ];
